6c1966ceb37e26064855ef40d3f623dfbb3d010f
saxenasaksham46 <saxenasaksham46>
2025-04-25 00:00:22 +0530

    Added Navbar Component

--- email-flow-frontend/src/App.tsx
+++ email-flow-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { Toaster } from "react-hot-toast"
 import Routing from "./routes/Routing"
+import Navbar from "./base/Navbar";
 const App = () => {
   return (
     <React.Fragment>
+      
     <Toaster />
+    <Navbar />
     <Routing />
     </React.Fragment>
   )
